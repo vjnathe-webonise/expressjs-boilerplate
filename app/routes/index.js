45f9f0de885d4e2333d535b5369dd42b7ee82a79
vijayraj <vijayraj.nathe@weboniselab.com>
2018-04-26 17:04:13 +0530

    added logger for 404

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var userRoutes = require('./users');
+var winston = require('../../configs/winston-config.js');
 
 router.get('/', (req, res) => {
     res.render('index', { title: 'API', message: "specify request uri to access other api's" })
@@ -9,6 +10,7 @@ router.get('/', (req, res) => {
 router.use('/user', userRoutes);
 
 router.all('*', (req, res) => {
+    winston.error(`${404} - ${req.originalUrl} - ${req.method} - ${req.ip}`);
     res.status(404).send('Not found');
 });
 
